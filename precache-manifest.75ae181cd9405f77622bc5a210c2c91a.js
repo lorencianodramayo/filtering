a580017e5fa2a54e4306808392b626a925effffd
Lorenciano Dramayo <lorencianodramayo@Cianos-Macbook-Air.local>
2020-04-01 21:49:17 +0800

    Updates

---
+++ precache-manifest.75ae181cd9405f77622bc5a210c2c91a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942ff0d582e3274a73a1fe77a177952a",
+    "url": "/filtering/index.html"
+  },
+  {
+    "revision": "2aa7c3ce1081a57df3a7",
+    "url": "/filtering/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "bd3c2dc7c4d976f7a30f",
+    "url": "/filtering/static/css/main.5ff0a2a0.chunk.css"
+  },
+  {
+    "revision": "2aa7c3ce1081a57df3a7",
+    "url": "/filtering/static/js/2.3d781cf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filtering/static/js/2.3d781cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3c2dc7c4d976f7a30f",
+    "url": "/filtering/static/js/main.3b048c59.chunk.js"
+  },
+  {
+    "revision": "ef0b31e20e2ccc9e83da",
+    "url": "/filtering/static/js/runtime-main.ef5d3ee9.js"
+  }
+]);
